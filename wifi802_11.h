f64709af9072dd331d5efb0833045ede9a8129c6
Perotto <julianoperotto@gmail.com>
2020-08-05 15:46:13 -0300

    Migracao e ajustes de formatacao

--- wifi802_11.h
+++ wifi802_11.h
@@ -4,6 +4,6 @@
 
 void wifi_802_11_begin(char bsId[], int channel);
 void wifi_802_11_send(const uint8_t *d, int len);
-void wifi_802_receive_cb(void(*cb)(const uint8_t *, int, uint8_t));
+void wifi_802_receive_cb(void (*cb)(const uint8_t *, int, uint8_t));
 
 #endif
